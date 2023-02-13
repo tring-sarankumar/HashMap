2f964d259cb7caf469c06ad0883be0d34247cae8
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-02-13 10:51:26 +0530

    commit

--- src/main/java/assignment/hashmap/Hashset.java
+++ src/main/java/assignment/hashmap/Hashset.java
@@ -8,7 +8,7 @@ public class Hashset {
 
 	public static void main(String[] args) {
 		//Creating HashSet and adding elements
-		 Logger log =  Logger.getLogger("CreditCard");
+		 Logger l =  Logger.getLogger("CreditCard");
 		 HashSet<String> set=new HashSet<String>();  
          set.add("One");    
          set.add("Two");    
@@ -19,14 +19,14 @@ public class Hashset {
          Iterator<String> i=set.iterator();  
          while(i.hasNext())  
          {  
-         log.info(i.next());  
+	     l.log(Level.INFO,()->" " +i.next()); 
          }  
          
          HashSet<String> set1=new HashSet<String>();  
          set1.add("Ten");  
          set1.add("Twenty");  
-         set.addAll(set1);  
-         log.info("Updated List: "+set);
+         set.addAll(set1);
+	 l.log(Level.INFO,()->" " +set);
          
          //Removing specific element from HashSet  
          set.remove("one"); 
